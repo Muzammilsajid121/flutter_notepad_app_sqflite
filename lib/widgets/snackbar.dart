dd94d8fa82fe74cbb3bc8dd0fdc95dc365bbc45e
Muzammil Sajid <pc.21695.muzammil@gmail.com>
2024-05-26 23:29:51 +0500

    custom snackbar

---
+++ lib/widgets/snackbar.dart
@@ -0,0 +1,44 @@
+// import 'package:flutter/material.dart';
+
+// class CustomSnackbar extends StatefulWidget {
+//   final String title;
+//   const CustomSnackbar({super.key, required this.title});
+
+//   @override
+//   State<CustomSnackbar> createState() => _CustomSnackbarState();
+// }
+
+// class _CustomSnackbarState extends State<CustomSnackbar> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return  SnackBar(
+//       content: Text(widget.title),
+//       backgroundColor: Colors.red[50],
+//       elevation: 2,
+//       width: double.infinity,
+//       behavior: SnackBarBehavior.floating,
+//       shape: RoundedRectangleBorder(
+//         borderRadius: BorderRadius.circular(10.0),   ),
+        
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+SnackBar customSnackbar(String title) {
+  return SnackBar(
+    backgroundColor: const Color.fromARGB(255, 253, 183, 178),
+    elevation: 1,
+    shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(1.0), ),
+    duration: Duration(milliseconds: 800),
+
+    content: Text(title,
+      style: GoogleFonts.montserrat( // You can replace 'roboto' with any other Google Font
+        textStyle: const TextStyle(
+      color: Colors.black, fontSize: 17, fontWeight: FontWeight.w600,   ), )),
+
+
+  );
+}
