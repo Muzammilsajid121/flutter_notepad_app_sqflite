4930ccc359b53e272d1db81a3d05b010fe866132
Muzammil Sajid <pc.21695.muzammil@gmail.com>
2024-05-29 23:01:56 +0500

    snackbar

--- lib/widgets/snackbar.dart
+++ lib/widgets/snackbar.dart
@@ -1,29 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class CustomSnackbar extends StatefulWidget {
-//   final String title;
-//   const CustomSnackbar({super.key, required this.title});
-
-//   @override
-//   State<CustomSnackbar> createState() => _CustomSnackbarState();
-// }
-
-// class _CustomSnackbarState extends State<CustomSnackbar> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return  SnackBar(
-//       content: Text(widget.title),
-//       backgroundColor: Colors.red[50],
-//       elevation: 2,
-//       width: double.infinity,
-//       behavior: SnackBarBehavior.floating,
-//       shape: RoundedRectangleBorder(
-//         borderRadius: BorderRadius.circular(10.0),   ),
-        
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
